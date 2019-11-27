18979c164e315a732cef9a0162e0c95ee73f9c54
sergeinemogai <nemogaiergei1998@gmail.com>
2019-11-27 13:06:32 +0300

    Users entity class created Adding constructor, setters and getters methods

---
+++ src/main/java/entity/Users.java
@@ -0,0 +1,47 @@
+package entity;
+
+public class Users {
+    private int ID;
+    private String firstName;
+    private String lastName;
+    private String role;
+
+    public Users(int ID, String firstName, String lastName, String role) {
+        this.ID = ID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
