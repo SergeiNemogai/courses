877e782941551a43e9bcad0f5da9561a64b075c2
sergeinemogai <nemogaiergei1998@gmail.com>
2019-11-28 16:52:29 +0300

    Added DAO implementation for Role entity

---
+++ src/main/java/dao/RoleDAO.java
@@ -0,0 +1,32 @@
+package dao;
+
+import entity.Role;
+
+import java.util.List;
+
+public class RoleDAO implements DAO<Role> {
+    @Override
+    public void add(Role entity) {
+
+    }
+
+    @Override
+    public Role getByID(int ID) {
+        return null;
+    }
+
+    @Override
+    public List<Role> getAll() {
+        return null;
+    }
+
+    @Override
+    public void edit(Role entity) {
+
+    }
+
+    @Override
+    public void remove(Role entity) {
+
+    }
+}
