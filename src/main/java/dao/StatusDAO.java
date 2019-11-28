adb47c41c1c3250ed7362a64a1d0001acea98ff6
sergeinemogai <nemogaiergei1998@gmail.com>
2019-11-28 17:02:33 +0300

    Added DAO implementation for Status entity

---
+++ src/main/java/dao/StatusDAO.java
@@ -0,0 +1,32 @@
+package dao;
+
+import entity.Status;
+
+import java.util.List;
+
+public class StatusDAO implements DAO<Status> {
+    @Override
+    public void add(Status entity) {
+
+    }
+
+    @Override
+    public Status getByID(int ID) {
+        return null;
+    }
+
+    @Override
+    public List<Status> getAll() {
+        return null;
+    }
+
+    @Override
+    public void edit(Status entity) {
+
+    }
+
+    @Override
+    public void remove(Status entity) {
+
+    }
+}
