4cc7e0298e96ca15d81195bde64b19fa6a0e08bd
sergeinemogai <nemogaiergei1998@gmail.com>
2019-11-28 16:59:53 +0300

    Implemented all of the RoleDAO methods

--- src/main/java/dao/RoleDAO.java
+++ src/main/java/dao/RoleDAO.java
@@ -1,13 +1,28 @@
 package dao;
 
+import datasource.HikariCPDataSource;
+import entity.CreatedCourse;
 import entity.Role;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RoleDAO implements DAO<Role> {
     @Override
     public void add(Role entity) {
-
+        try {
+            Connection connection = HikariCPDataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "insert into courses.roles value ?");
+            preparedStatement.setString(1, entity.getRole());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -17,16 +32,28 @@ public class RoleDAO implements DAO<Role> {
 
     @Override
     public List<Role> getAll() {
-        return null;
+        List<Role> roles = new ArrayList<>();
+        try {
+            Connection connection = HikariCPDataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "select * from courses.roles");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                roles.add(new Role(resultSet.getString(1)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return roles;
     }
 
     @Override
     public void edit(Role entity) {
-
+        // no sense to change role
     }
 
     @Override
     public void remove(Role entity) {
-
+        // no sense to remove role
     }
 }
