7c03c434d01c54d6e82803467e4f5390c45ef81a
sergeinemogai <nemogaiergei1998@gmail.com>
2019-11-28 17:14:13 +0300

    Added DAO implementation for Study entity

---
+++ src/main/java/dao/StudyDAO.java
@@ -0,0 +1,32 @@
+package dao;
+
+import entity.Study;
+
+import java.util.List;
+
+public class StudyDAO implements DAO<Study> {
+    @Override
+    public void add(Study entity) {
+
+    }
+
+    @Override
+    public Study getByID(int ID) {
+        return null;
+    }
+
+    @Override
+    public List<Study> getAll() {
+        return null;
+    }
+
+    @Override
+    public void edit(Study entity) {
+
+    }
+
+    @Override
+    public void remove(Study entity) {
+
+    }
+}
