37dbba92f91621efc0e6cc929575a05c22068f23
sergeinemogai <nemogaiergei1998@gmail.com>
2019-11-28 16:17:18 +0300

    Add DAO implementation for CreatedCourse entity

---
+++ src/main/java/dao/CreatedCourseDAO.java
@@ -0,0 +1,32 @@
+package dao;
+
+import entity.CreatedCourse;
+
+import java.util.List;
+
+public class CreatedCourseDAO implements DAO<CreatedCourse> {
+    @Override
+    public void add(CreatedCourse entity) {
+
+    }
+
+    @Override
+    public CreatedCourse getByID(int ID) {
+        return null;
+    }
+
+    @Override
+    public List<CreatedCourse> getAll() {
+        return null;
+    }
+
+    @Override
+    public void edit(CreatedCourse entity) {
+
+    }
+
+    @Override
+    public void remove(CreatedCourse entity) {
+
+    }
+}
