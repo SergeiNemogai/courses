a64af17378261f59db7d8ba9be280d030f6645ac
sergeinemogai <nemogaiergei1998@gmail.com>
2019-11-28 15:43:38 +0300

    CourseDAO implementation of DAO for Course Methods still empty

---
+++ src/main/java/dao/CourseDAO.java
@@ -0,0 +1,32 @@
+package dao;
+
+import entity.Course;
+
+import java.util.List;
+
+public class CourseDAO implements DAO<Course> {
+    @Override
+    public void add(Course entity) {
+
+    }
+
+    @Override
+    public Course getByID(int ID) {
+        return null;
+    }
+
+    @Override
+    public List<Course> getAll() {
+        return null;
+    }
+
+    @Override
+    public void edit(Course entity) {
+
+    }
+
+    @Override
+    public void remove(Course entity) {
+
+    }
+}
