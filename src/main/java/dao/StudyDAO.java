c610a16c29d056a74eb150e37bfe778c885679cd
sergeinemogai <nemogaiergei1998@gmail.com>
2019-11-28 17:28:12 +0300

    Implemented all of the StudyDAO methods

--- src/main/java/dao/StudyDAO.java
+++ src/main/java/dao/StudyDAO.java
@@ -1,32 +1,95 @@
 package dao;
 
+import datasource.HikariCPDataSource;
 import entity.Study;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudyDAO implements DAO<Study> {
     @Override
     public void add(Study entity) {
-
+        try {
+            Connection connection = HikariCPDataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "insert into courses.study values(?, ?, ?)");
+            preparedStatement.setInt(1, entity.getID());
+            preparedStatement.setInt(2, entity.getCourseID());
+            preparedStatement.setInt(3, entity.getUserID());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public Study getByID(int ID) {
-        return null;
+        Study study = null;
+        try {
+            Connection connection = HikariCPDataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "select * from courses.study where id=?");
+            preparedStatement.setInt(1, ID);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()) {
+                study = new Study(resultSet.getInt(1),
+                        resultSet.getInt(2),
+                        resultSet.getInt(3));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return study;
     }
 
     @Override
     public List<Study> getAll() {
-        return null;
+        List<Study> studies = new ArrayList<>();
+        try {
+            Connection connection = HikariCPDataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "select * from courses.study");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                studies.add(new Study(resultSet.getInt(1),
+                        resultSet.getInt(2),
+                        resultSet.getInt(3)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return studies;
     }
 
     @Override
     public void edit(Study entity) {
-
+        try {
+            Connection connection = HikariCPDataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "update courses.study set user_id=?, course_id=? where id=?");
+            preparedStatement.setInt(1, entity.getUserID());
+            preparedStatement.setInt(2, entity.getCourseID());
+            preparedStatement.setInt(3, entity.getID());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void remove(Study entity) {
-
+        try {
+            Connection connection = HikariCPDataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "delete from courses.study where id=?");
+            preparedStatement.setInt(1, entity.getID());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
