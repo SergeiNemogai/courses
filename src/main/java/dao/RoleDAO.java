906df1636c2770b9f53a0f7ca8646dc498321c0a
sergeinemogai <nemogaiergei1998@gmail.com>
2019-11-29 17:23:02 +0300

    RoleDAO has been refactored by using try-with-resources

--- src/main/java/dao/RoleDAO.java
+++ src/main/java/dao/RoleDAO.java
@@ -13,10 +13,9 @@ import java.util.List;
 public class RoleDAO implements DAO<Role> {
     @Override
     public void add(Role entity) {
-        try {
-            Connection connection = HikariCPDataSource.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(
-                    "insert into courses.roles value ?");
+        try (Connection connection = HikariCPDataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "insert into courses.roles value ?")) {
             preparedStatement.setString(1, entity.getRole());
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
@@ -32,13 +31,13 @@ public class RoleDAO implements DAO<Role> {
     @Override
     public List<Role> getAll() {
         List<Role> roles = new ArrayList<>();
-        try {
-            Connection connection = HikariCPDataSource.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(
-                    "select * from courses.roles");
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                roles.add(new Role(resultSet.getString(1)));
+        try (Connection connection = HikariCPDataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "select * from courses.roles")) {
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    roles.add(new Role(resultSet.getString(1)));
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
