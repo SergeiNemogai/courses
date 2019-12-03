f398fdbe571d266187838df074298b786c9456c0
sergeinemogai <nemogaiergei1998@gmail.com>
2019-12-03 14:43:55 +0300

    Added methods rollbackConnection and closeConnection Connections are now closed manually in the finally block

--- src/main/java/service/CourseService.java
+++ src/main/java/service/CourseService.java
@@ -11,66 +11,104 @@ import java.util.List;
 
 public class CourseService {
     private CourseDAO courseDAO;
+    private Connection connection;
 
-    public void add(Course entity) {
-        try (Connection connection = HikariCPDataSource.getConnection()) {
-            courseDAO = DAOFactory.getCourseDAO();
-            connection.setAutoCommit(false);
-            courseDAO.add(entity, connection);
-            connection.commit();
+    private void rollbackConnection(Connection connection) {
+        try {
+            connection.rollback();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
+    private void closeConnection(Connection connection) {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void add(Course entity) {
+        try {
+            connection = HikariCPDataSource.getConnection();
+            connection.setAutoCommit(false);
+            courseDAO = DAOFactory.getCourseDAO();
+            courseDAO.add(entity, connection);
+            connection.commit();
+        } catch (SQLException e) {
+            rollbackConnection(connection);
+            e.printStackTrace();
+        } finally {
+            closeConnection(connection);
+        }
+    }
+
     public Course getById(int id) {
-        try (Connection connection = HikariCPDataSource.getConnection()) {
+        try {
+            connection = HikariCPDataSource.getConnection();
             courseDAO = DAOFactory.getCourseDAO();
             return courseDAO.getById(id, connection);
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            closeConnection(connection);
         }
         return null;
     }
 
     public List<Course> getAll() {
-        try (Connection connection = HikariCPDataSource.getConnection()) {
+        try {
+            connection = HikariCPDataSource.getConnection();
             courseDAO = DAOFactory.getCourseDAO();
             return courseDAO.getAll(connection);
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            closeConnection(connection);
         }
         return null;
     }
 
     public void edit(Course entity) {
-        try (Connection connection = HikariCPDataSource.getConnection()) {
-            courseDAO = DAOFactory.getCourseDAO();
+        try {
+            connection = HikariCPDataSource.getConnection();
             connection.setAutoCommit(false);
+            courseDAO = DAOFactory.getCourseDAO();
             courseDAO.edit(entity, connection);
             connection.commit();
         } catch (SQLException e) {
+            rollbackConnection(connection);
             e.printStackTrace();
+        } finally {
+            closeConnection(connection);
         }
     }
 
     public void remove(Course entity) {
-        try (Connection connection = HikariCPDataSource.getConnection()) {
-            courseDAO = DAOFactory.getCourseDAO();
+        try {
+            connection = HikariCPDataSource.getConnection();
             connection.setAutoCommit(false);
+            courseDAO = DAOFactory.getCourseDAO();
             courseDAO.remove(entity, connection);
             connection.commit();
         } catch (SQLException e) {
+            rollbackConnection(connection);
             e.printStackTrace();
+        } finally {
+            closeConnection(connection);
         }
     }
 
     public List<Course> getCoursesOnStudy() {
-        try (Connection connection = HikariCPDataSource.getConnection()) {
+        try {
+            connection = HikariCPDataSource.getConnection();
             courseDAO = DAOFactory.getCourseDAO();
             return courseDAO.getCoursesOnStudy(connection);
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            closeConnection(connection);
         }
         return null;
     }
